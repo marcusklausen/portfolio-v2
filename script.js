e91ce3cb6463c09fd091f465dfda54c978dde489
Marcus Klausen <hello@marcusklausen.dk>
2021-05-04 20:51:40 +0200

    added breakpoints up to 1800 need more as I'm using padding instead of margin:auto

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ let header = document.querySelector("#header");
 
 
 
+
+
 document.addEventListener("scroll", () => {
-    let hero = document.querySelector("#hero");
-    let heroRect = hero.getBoundingClientRect();
-    console.log("top", heroRect.bottom)
 
-    if (heroRect.bottom < 58) {
-        header.style = "animation: full-width-menu 2000ms ease infinite alternate;"
-        header.classList.add('header-br');
-    }
+    
+        let hero = document.querySelector("#hero");
+        let heroRect = hero.getBoundingClientRect();
+        console.log("top", heroRect.bottom)
+    
+        if (heroRect.bottom < 110) {
 
-    if (heroRect.bottom > 58) {
-        header.style = "grid-template-columns: 66% auto"
-        header.classList.remove('header-br');
-    }
-  });
+            if(document.documentElement.clientWidth >= 990) {
+
+                header.style = "width:100%; background-color: #3b765f; padding-right:4em;"
+                header.classList.add('header-br');
+            }
+
+            if(document.documentElement.clientWidth >= 1390) {
+                header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:12em;");
+            }
+
+            if(document.documentElement.clientWidth >= 1800) {
+                header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:25em;");
+            }
+        }
+    
+        if (heroRect.bottom > 58) {
+            header.style = "width:66%;"
+            header.classList.remove('header-br');
+        }
+
+        
+
+
+    
+});

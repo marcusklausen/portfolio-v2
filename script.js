4c7b9e00fa646d85bd77b51a440a64e5fa221c09
Marcus Klausen <hello@marcusklausen.dk>
2021-05-06 13:10:44 +0200

    adding cards now

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 
 let header = document.querySelector("#header");
 
-
-
-
+let hero = document.querySelector("#hero");
 
 document.addEventListener("scroll", () => {
+    
+    let heroRect = hero.getBoundingClientRect();
+    
+        console.log("bottom", heroRect.bottom)
 
-    
-        let hero = document.querySelector("#hero");
-        let heroRect = hero.getBoundingClientRect();
-        console.log("top", heroRect.bottom)
-    
-        if (heroRect.bottom < 110) {
+        
+        if (heroRect.bottom < 80) {
 
             if(document.documentElement.clientWidth >= 990) {
 
-                header.style = "width:100%; background-color: #3b765f; padding-right:4em;"
+                header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:4em;");
                 header.classList.add('header-br');
             }
 
             if(document.documentElement.clientWidth >= 1390) {
                 header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:12em;");
+                header.classList.add('header-br');
             }
 
+            
+
+            
+
             if(document.documentElement.clientWidth >= 1800) {
-                header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:25em;");
+                header.setAttribute("style", "width:100%; background-color: #3b765f; padding-right:15em;");
+                header.classList.add('header-br');
             }
-        }
-    
-        if (heroRect.bottom > 58) {
+
+            
+
+            
+        } else if (heroRect.bottom > 80 && document.documentElement.clientWidth >= 960) {
             header.style = "width:66%;"
             header.classList.remove('header-br');
-        }
+        } 
+        
+        
 
         
 
